4d9dded2537b281d83db436128b74207220dc430
Sam Guan <xin.guan@gmail.com>
2022-12-20 09:03:33 -0800

    add server change

--- server/index.js
+++ server/index.js
@@ -7,8 +7,6 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
@@ -18,7 +16,7 @@ app.use('/posts', postRoutes);
 const CONNECTION_URL = 'mongodb+srv://dbadmin:Dbadmin123@cluster0.oqvj95y.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
