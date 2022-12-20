0d4e741593ca9ce727276e87b6bc339aa315e079
Nathan Guan <nathanguan2009@gmail.com>
2022-12-20 09:03:33 -0800

    add server change

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,5 @@
 import PostMessage from '../models/postMessage.js';
+import mongoose from 'mongoose';
 
 export const getPosts = async (req, res) => { 
   try {
@@ -7,6 +8,18 @@ export const getPosts = async (req, res) => {
       console.log(postMessages);
 
       res.status(200).json(postMessages);
+  } catch (error) {
+      res.status(404).json({ message: error.message });
+  }
+}
+
+export const getPost = async (req, res) => { 
+  const { id } = req.params;
+
+  try {
+      const post = await PostMessage.findById(id);
+      
+      res.status(200).json(post);
   } catch (error) {
       res.status(404).json({ message: error.message });
   }
@@ -24,4 +37,15 @@ export const createPost = async (req, res) => {
   } catch (error) {
       res.status(409).json({ message: error.message });
   }
+}
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  
+if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+
+  res.json(updatedPost);
 }
