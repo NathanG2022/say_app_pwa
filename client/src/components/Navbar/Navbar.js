04bf08dd401cf35880c04e26f3f3d037c7c9e804
Nathan Guan <nathanguan2009@gmail.com>
2022-12-22 15:06:44 -0800

    manual sign in/up completed

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useHistory, useLocation } from 'react-router-dom';
 import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import {  } from 'react-router-dom';
 import decode from 'jwt-decode';
 
-import useStyles from './sytles';
 import say from '../../images/say.jpg';
+import * as actionType from '../../constants/actionTypes';
+import useStyles from './styles';
 
 const Navbar = () => {
-  const classes = useStyles();
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
   const dispatch = useDispatch();
-  const history = useHistory();
   const location = useLocation();
+  const history = useHistory();
+  const classes = useStyles();
 
   const logout = () => {
-    dispatch({ type: "LOGOUT" });
+    dispatch({ type: actionType.LOGOUT });
 
     history.push('/auth');
 
@@ -26,11 +26,11 @@ const Navbar = () => {
   useEffect(() => {
     const token = user?.token;
 
-    // if (token) {
-    //   const decodedToken = decode(token);
+    if (token) {
+      const decodedToken = decode(token);
 
-    //   if (decodedToken.exp * 1000 < new Date().getTime()) logout();
-    // }
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
 
     setUser(JSON.parse(localStorage.getItem('profile')));
   }, [location]);
@@ -42,10 +42,10 @@ const Navbar = () => {
         <img className={classes.image} src={say} alt="icon" height="60" />
       </div>
       <Toolbar className={classes.toolbar}>
-        {user ? (
+        {user?.result ? (
           <div className={classes.profile}>
             <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
-            <Typography component={Link} to="/" className={classes.userName} variant="h6">{user?.result.name}</Typography>
+            <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
             <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
           </div>
         ) : (
@@ -53,7 +53,7 @@ const Navbar = () => {
         )}
       </Toolbar>
     </AppBar>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
