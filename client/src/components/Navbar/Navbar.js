802d1015a282af6cb2636864acd21ee3497e72b7
Sam Guan <xin.guan@gmail.com>
2022-12-21 20:01:05 -0800

    google auth not working though

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import {  } from 'react-router-dom';
+import decode from 'jwt-decode';
 
 import useStyles from './sytles';
 import say from '../../images/say.jpg';
 
 const Navbar = () => {
   const classes = useStyles();
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const location = useLocation();
 
-  const user = null;
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+
+    history.push('/auth');
+
+    setUser(null);
+  };
+
+  useEffect(() => {
+    const token = user?.token;
+
+    // if (token) {
+    //   const decodedToken = decode(token);
+
+    //   if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    // }
+
+    setUser(JSON.parse(localStorage.getItem('profile')));
+  }, [location]);
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
@@ -21,7 +46,7 @@ const Navbar = () => {
           <div className={classes.profile}>
             <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
             <Typography component={Link} to="/" className={classes.userName} variant="h6">{user?.result.name}</Typography>
-            <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
           </div>
         ) : (
           <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
