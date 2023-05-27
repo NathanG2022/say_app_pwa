f73c6eeaca553ab4ddc94acbd8b378791d8a5aaf
Xin Guan <xin.guan@gmai.com>
2023-05-27 11:57:32 -0700

    UI changes by Sam

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -43,8 +43,8 @@ const Navbar = () => {
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
       <Link to="/" className={classes.brandContainer}>
-        <img component={Link} to="/" src={sayText} alt="icon" height="45px" />
-        <img className={classes.image} src={sayLogo} alt="icon" height="40px" />
+        <img component={Link} to="/" src={sayText} alt="icon" height="30px" />
+        <img className={classes.image} src={sayLogo} alt="icon" height="60px" />
       </Link>
       <Toolbar className={classes.toolbar}>
         {user?.result ? (
