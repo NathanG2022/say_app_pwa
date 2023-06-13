781ae85c3a182bffd8307de2b72fe263b32ae60c
Dylan <dylan.zhuang@ufl.edu>
2023-06-13 18:36:57 -0400

    Refactor messages controller:
    Convert require statements to import statements
    Update schema and model export
    Implement getMessages and sendMessage functions with appropriate error handling

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -1,21 +1,33 @@
-const mongoose = require("mongoose");
+import express from 'express';
+import mongoose from 'mongoose';
 
-const messageSchema = new mongoose.Schema({
-  author: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-  },
-  recipient: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-  },
-  content: {
-    type: String,
-  },
-  date: {
-    type: Date,
-  },
-  type: { type: String },
-});
+import messages from '../models/messages';
 
-module.exports = mongoose.model("Message", messageSchema);
+const router = express.Router();
+
+export const getMessages = async (req, res) => {
+    try {
+        const messages = await messages.find();
+        const recipient = req.body.recipient;
+        const sender = req.body.sender;
+        const filteredMessages = messages.filter(message => message.recipient === recipient && message.sender === sender || message.recipient === sender && message.sender === recipient);
+        //returns all messages where the recipient is the recipient and the sender is the sender or the recipient is the sender and the sender is the recipient
+        res.status(200).json(filteredMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const sendMessage = async (req, res) => {
+    const message = req.body;
+
+    const newMessage = new messages(message);
+
+    try {
+        await newMessage.save();
+
+        res.status(201).json(newMessage);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
