204147170d4bb71dc5fca592a07c65e2ca5f38fc
Dylan Zhuang <dylan.zhuang@ufl.edu>
2023-07-01 02:46:38 -0400

    Refactor server codebase Add new routes and controllers for direct messages and user functionality Integrate helmet for enhanced security Implement xss-clean and express-mongo-sanitize for user input sanitization Introduce express-ws for WebSocket support Update package dependencies to latest versions

--- server/index.js
+++ server/index.js
@@ -3,10 +3,16 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
+import messageRouter from "./routes/messages.js";
+import directMessageRouter from "./routes/directMessages.js";
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
-import messageRouter from "./routes/messages.js";
+
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+import http from "http";
+
 
 const app = express();
 dotenv.config({path:'.env'});
@@ -14,14 +20,20 @@ dotenv.config({path:'.env'});
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 app.use("/messages", messageRouter);
-
+app.use("/directmessages", directMessageRouter);
 app.get('/', (req, res) => {
   res.send('APP IS RUNNING');
 })
+
+// const server = http.createServer(app);
+// socketServer.registerSocketServer(server);
 
 const PORT = process.env.PORT|| 5000;
 
--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { getMessages, sendMessage } from '../controllers/messages.js';
+import { getMessages, sendMessage, getUsers} from '../controllers/messages.js';
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get('/get',auth, getMessages);
+router.get('/get/:startindex/:endindex',auth, getMessages);
 router.post('/send',auth, sendMessage);
+router.get('/getUsers',auth, getUsers);
 
 export default router;
