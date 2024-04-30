579b52efac4030fb44418b25bcbd8aaec2d17d4a
Xin Guan <xin.guan@gmail.com>
2024-04-29 17:35:05 -0700

    serviceworker.js change

--- client/src/serviceWorkerRegistration.js
+++ client/src/serviceWorkerRegistration.js
@@ -30,7 +30,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/serviceworker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
@@ -119,8 +119,8 @@ function checkValidServiceWorker(swUrl, config) {
         registerValidSW(swUrl, config);
       }
     })
-    .catch(() => {
-      console.log('No internet connection found. App is running in offline mode.');
+    .catch((error) => {
+      console.log('No internet connection found. App is running in offline mode.', error);
     });
 }
 
